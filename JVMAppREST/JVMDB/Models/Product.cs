4f3f10aeabc26d67a982ab91db191402dfd0cbff
mannj2015 <72755084+mannj2015@users.noreply.github.com>
2020-11-12 10:18:00 -0500

    added context

--- JVMAppREST/JVMDB/Models/Product.cs
+++ JVMAppREST/JVMDB/Models/Product.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace JVMDB.Models
 {
     public class Product
     {
+        [Key]
         public int id { get; set; }
         public decimal cost { get; set; }
         public Category category { get; set; }
--- JVMAppREST/JVMDB/Repos/DBRepo.cs
+++ JVMAppREST/JVMDB/Repos/DBRepo.cs
@@ -1,10 +1,92 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using JVMDB.Models;
 
 namespace JVMDB.Repos
 {
-    class DBRepo
+    public class DBRepo : IProductRepo, IUserRepo, IUserProductRepo
     {
+
+
+        public void AddProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddUserProduct(UserProduct userProduct)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteUserProduct(UserProduct userProduct)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAllProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<UserProduct> GetAllUserProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<User> GetAllUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product GetProductById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetUserById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserProduct GetUserProduct(int userId, int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserProduct GetUserProductById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateUserProduct(UserProduct userProduct)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
