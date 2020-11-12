3a0ba5198cae2ed720426d724cf8a7b7f11ee4ab
Jacob <jacobjennings510@gmail.com>
2020-11-12 06:53:14 -0800

    Added models

---
+++ JVMAppREST/JVMDB/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace JVMDB.Models
+{
+    public class Product
+    {
+        public int id { get; set; }
+        public decimal cost { get; set; }
+        public Category category { get; set; }
+        public enum Category
+        {
+            Beverage,
+            Bread,
+            CannedGoods,
+            Dairy,
+            BakingGoods,
+            FrozenFood,
+            Meat,
+            Produce
+        }
+    }
+}
---
+++ JVMAppREST/JVMDB/Models/UserProduct.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace JVMDB.Models
+{
+    public class UserProduct
+    {
+        public int id { get; set; }
+        public int userId { get; set; }
+        public int productId { get; set; }
+    }
+}
