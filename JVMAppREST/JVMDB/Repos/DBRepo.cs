451253bed521df5bb0d3ecb25b544e6e917711a9
Jacob <jacobjennings510@gmail.com>
2020-11-12 07:04:45 -0800

    Repo methods created but not implemented

--- JVMAppREST/JVMDB/Repos/DBRepo.cs
+++ JVMAppREST/JVMDB/Repos/DBRepo.cs
@@ -1,10 +1,99 @@
-﻿using System;
+﻿using JVMDB.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace JVMDB.Repos
 {
-    class DBRepo
+    class DBRepo : IProductRepo, IUserProductRepo, IUserRepo
     {
+        #region Product Methods
+        public void AddProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+        public void DeleteProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+        public List<Product> GetAllProducts()
+        {
+            throw new NotImplementedException();
+        }
+        public Product GetProductById(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public void UpdateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+        #region User Methods
+        public void AddUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+        public void DeleteUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+        public List<User> GetAllUsers()
+        {
+            throw new NotImplementedException();
+        }
+        public User GetUserById(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public void AddUserProduct(UserProduct userProduct)
+        {
+            throw new NotImplementedException();
+        }
+        public void UpdateUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+        #region UserProduct Methods
+
+        public void DeleteUserProduct(UserProduct userProduct)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+
+        public List<UserProduct> GetAllUserProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        
+
+        
+
+        
+
+        public UserProduct GetUserProduct(int userId, int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserProduct GetUserProductById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+
+        
+
+        public void UpdateUserProduct(UserProduct userProduct)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
     }
 }
